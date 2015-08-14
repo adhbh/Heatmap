464b93c989526974824570eabdaa9c54e43592df
Gerardo Renovales <gerardo-renovales@MAC-C02PNYUHG8WN.local>
2015-08-14 15:32:01 -0400

    Add mongo connection support, and read from mongo collection

--- app.js
+++ app.js
@@ -5,8 +5,12 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var mongo = require('mongodb');
+var monk = require('monk');
+var db = monk('localhost:27017/heatmap');
+
 var routes = require('./routes/index');
-var users = require('./routes/users');
+
 
 var app = express();
 
@@ -22,8 +26,13 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Make our db accessible to our router
+app.use(function(req,res,next){
+    req.db = db;
+    next();
+});
+
 app.use('/', routes);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,19 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* Get Heatmap data */
+router.get('/server/', function(req, res,next) {
+  var db = req.db;
+    var collection = db.get('mouseTrack');
+    collection.find({},{},function(e,docs){
+        res.json(docs);
+    });  
+});
+
+/* Post Heatmap Data */
+router.post('/server', function(req, res) {
+  var db = req.db;
+  
+});
+
 module.exports = router;
